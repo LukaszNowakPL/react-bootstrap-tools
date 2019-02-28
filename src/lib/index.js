d2281cecfe0a932ad043801714a181ad7e2f3292
lukasz.nowak.at.phyzzi.com <lukasz.nowak@phyzzi.com>
2019-02-28 18:06:10 +0100

    Example component removal

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,5 @@
-// import Example from './components/Example';
 import DescribedComponent from "./components/DescribedComponent";
 import DescribedButton from "./components/DescribedButton";
 import Glyphicon from "./components/Glyphicon";
-
-// export default Example;
 
 export { DescribedComponent, DescribedButton, Glyphicon };
